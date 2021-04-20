a3f8f33c65266b97ed6b346112e699cdb9aa541d
Andy Pearson <andy@londonparkour.com>
2021-04-20 07:39:30 +0100

    Add credentials_test.json git-crypted

--- src/initialise.php
+++ src/initialise.php
@@ -11,8 +11,8 @@ class initialise {
         // ┌─────────────────────────────────────────────────────────────────────────┐
         // │                            Include Field Groups    	        	     │
         // └─────────────────────────────────────────────────────────────────────────┘
-        require __DIR__.'/acf/acf_field_groups.php';
         require __DIR__.'/acf/acf_admin_page.php';
+        require __DIR__.'/acf/acf_field_groups.php';
         require __DIR__.'/acf/acf_on_update.php';
 
         //  ┌─────────────────────────────────────────────────────────────────────────┐
