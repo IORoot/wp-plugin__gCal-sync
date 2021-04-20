b0f18210ec2dda67665c332e30ac00527ce26899
Andy Pearson <andy@londonparkour.com>
2021-04-20 13:04:06 +0100

    add icon

--- src/acf/acf_admin_page.php
+++ src/acf/acf_admin_page.php
@@ -7,12 +7,11 @@ function menu_gcal_importer(){
 
     if (function_exists('acf_add_options_page')) {
         $args = array(
-    
             /* (string) The title displayed on the options page. Required. */
-            'page_title' => 'gCal Importer',
+            'page_title' => '<svg viewBox="0 0 24 24" style="height:1.3em; vertical-align:text-bottom; fill:#1E3A8A;" xmlns="http://www.w3.org/2000/svg"><path d="M19,20V9H5V20H19M16,2H18V4H19A2,2 0 0,1 21,6V20A2,2 0 0,1 19,22H5A2,2 0 0,1 3,20V6A2,2 0 0,1 5,4H6V2H8V4H16V2M12,18.17L11.42,17.64C9.36,15.77 8,14.54 8,13.03C8,11.8 8.97,10.83 10.2,10.83C10.9,10.83 11.56,11.15 12,11.66C12.44,11.15 13.1,10.83 13.8,10.83C15.03,10.83 16,11.8 16,13.03C16,14.54 14.64,15.77 12.58,17.64L12,18.17Z"/></svg> gCal Importer',
             
             /* (string) The title displayed in the wp-admin sidebar. Defaults to page_title */
-            'menu_title' => 'gCal Importer',
+            'menu_title' => '<svg viewBox="0 0 24 24" style="height:1.3em; vertical-align:text-bottom; fill:#1E3A8A;" xmlns="http://www.w3.org/2000/svg"><path d="M19,20V9H5V20H19M16,2H18V4H19A2,2 0 0,1 21,6V20A2,2 0 0,1 19,22H5A2,2 0 0,1 3,20V6A2,2 0 0,1 5,4H6V2H8V4H16V2M12,18.17L11.42,17.64C9.36,15.77 8,14.54 8,13.03C8,11.8 8.97,10.83 10.2,10.83C10.9,10.83 11.56,11.15 12,11.66C12.44,11.15 13.1,10.83 13.8,10.83C15.03,10.83 16,11.8 16,13.03C16,14.54 14.64,15.77 12.58,17.64L12,18.17Z"/></svg> gCal Importer',
             
             /* (string) The URL slug used to uniquely identify this options page.
             Defaults to a url friendly version of menu_title */
